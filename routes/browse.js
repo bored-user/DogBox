c83f23fcdb9717aecbe90c7b6ca32ddc91469974
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-06-22 07:18:32 -0300

    Add subtitle & media cover support

--- routes/browse.js
+++ routes/browse.js
@@ -11,23 +11,52 @@ router.get('/browse/:type*', ({ originalUrl, params: { type } }, res) => {
             return;
         }
 
-        const files = [];
-        result.indexOf('.gitkeep') > -1 ? result.splice(result.indexOf('.gitkeep'), 1) : null;
+        const files = [],
+            gitkeep = result.indexOf('.gitkeep'),
+            png = result.indexOf('cover.png'),
+            jpg = result.indexOf('cover.jpg');
+
+        gitkeep > -1 ? result.splice(gitkeep, 1) : null;
+        png > -1 ? result.splice(png, 1) : null;
+        jpg > -1 ? result.splice(jpg, 1) : null;
 
         try {
             for (file of result) {
+                if (file.includes('vtt'))   continue;
+
                 const stats = fs.statSync(`${path}/${file}`);
                 if (stats === undefined || err) {
                     throw new Error();
                 }
 
-                files.push({ path: stats.isDirectory() ? `${originalUrl}/${file}` : `${originalUrl.replace('/browse/', '/watch/')}/${file}`, name: file });
+                if (stats.isDirectory()) {
+                    const temp = {
+                        path: `${originalUrl}/${file}`,
+                        name: `> ${file}`
+                    }
+
+                    if (fs.existsSync(`${path}/${file}/cover.jpg`)) {
+                        files.push(Object.assign(temp, { cover: `/assets/media/${originalUrl.replace('/browse/', '')}/${file}/cover.jpg`}));
+                    } else if (fs.existsSync(`${path}/${file}/cover.png`)) {
+                        files.push(Object.assign(temp, { cover: `/assets/media/${originalUrl.replace('/browse/', '')}/${file}/cover.png`}));
+                    } else {
+                        files.push(Object.assign(temp, { cover: false}));
+                    }
+                } else {
+                    let sub = false,
+                        path = `/assets/media/${originalUrl.replace('/browse/', '')}/${file.split('.')[0]}.vtt`;
+
+                    if (fs.existsSync(global.root + path))
+                        sub = path;
+
+                    files.push({ path: `${originalUrl.replace('/browse/', '/watch/')}/${file}`, name: file, sub: sub });
+                }
             };
         } catch (e) {
             res.render('errors/404');
         }
 
-        res.render('media', { files: files, type: type, accent: accent });
+        res.render('browse', { files: files, type: type, accent: accent });
     });
 });
 
--- routes/watch.js
+++ routes/watch.js
@@ -1,20 +1,21 @@
 const router = require('./_router'),
     fs = require('fs');
 
-router.get('/watch/:type/:file*', ({ originalUrl, params: { type, file } }, res) => {
+router.get('/watch/:type/:file*', ({ originalUrl, params: { type, file }, query }, res) => {
+    originalUrl = originalUrl.split('?')[0];
     const path = `${global.root}/assets/media/${originalUrl.replace('/watch/', '')}`;
     console.log(path);
 
     fs.stat(path, (err, stats) => {
-        if (stats === undefined)
+        if (stats === undefined || err)
             res.render('errors/404');
         else if (stats.isDirectory()) {
             fs.readdir(path, (err, files) => {
                 files = files.map(file => ({ name: file, path: path, type: type })).filter(file => file.name !== '.gitkeep');
-                res.render('media', { files: files, type: file, accent: type === 'shows' ? 'purple' : type === 'movies' ? 'yellow' : type === 'books' ? 'lightblue' : 'lightgreen' });
+                res.render('browse', { files: files, type: file, accent: type === 'shows' ? 'purple' : type === 'movies' ? 'yellow' : type === 'books' ? 'lightblue' : 'lightgreen' });
             });
         } else
-            res.render('watch', { uri: `/assets/media/${originalUrl.replace('/watch/', '')}`, tag: type === 'shows' || type === 'movies' ? 'video' : 'audio' })
+            res.render('watch', { sub: query.sub || false, uri: `/assets/media/${originalUrl.replace('/watch/', '')}`, tag: type === 'shows' || type === 'movies' ? 'video' : 'audio' })
     });
 });
 
